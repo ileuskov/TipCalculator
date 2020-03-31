7fcc0b5c2c6b44f0876b3d009bec10edb2afb010
ileuskov <ileuskov@gmail.com>
2020-03-31 16:05:45 +0200

    fixed the css design and js code

--- tip.js
+++ tip.js
@@ -1,31 +1,30 @@
-alert("hello");
+// Initializing the variables that store the input from a user
+var moneyAmount = document.querySelector("#moneyAmount");
+var serviceQuality = document.querySelector("#serviceQuality");
+var numOfPeople = document.querySelector("#peopleAmount");
+
+// Initializing a variable for a button and adding a click listener to it
+var tip_btn = document.querySelector("#calculate");
+tip_btn.addEventListener("click", calculateTip);
+
+
+
 
 // Calculating the tip
 function calculateTip() {
-    var moneyAmount = document.getElementById("moneyAmount").value;
-    var serviceQuality = document.getElementById("serviceQuality").value;
-    var numOfPeople = document.getElementById("PeopleAmount").value;
 
-if(moneyAmount === "" || serviceQuality == 0){
-    alert("Please enter the bill amount and the service ");
-    return;
-}
-alert(moneyAmount);
-alert(serviceQuality);
-alert(numOfPeople);
-alert(total);
-}
+    var cost = Number(moneyAmount.value);
+    var service = Number(serviceQuality.value);
+    var people = Number(numOfPeople.value);
+    
 
-//Calculate the tip
+     if (cost === 0 || service === 0 || people === 0) {
+         alert("Please enter the bill amount, service quality and the number of people ");
+         return;
+     }
 
-var total = (moneyAmount * serviceQuality) + numOfPeople;
+    var total = (cost * service) / people;
 
-
-//Display the tip
-document.getElementById("tip").innerHTML = total;
-
-//click to call the function
-
-document.getElementById("calculate").onclick = function() {
-    calculateTip();
+    var show = document.querySelector(".tip");
+    show.innerHTML = "€" + total.toFixed(2) + " each";
 };
